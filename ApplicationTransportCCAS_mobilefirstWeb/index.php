01d130a0c45fb66f7dd6c0d3a71775690c3cad6d
Salih <gungor.salih@outlook.fr>
2018-01-31 17:14:54 +0100

    appli mobile alibaba

---
+++ ApplicationTransportCCAS_mobilefirstWeb/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Mobile First Test</title>
+  <link rel="stylesheet" href="bootstrap-v3/css/bootstrap-theme.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One" rel="stylesheet">
+  <link rel="stylesheet" href="bootstrap-v3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="bootstrap-v3/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header" style="width:auto;">
+        <a href="index.php">
+          <img class="img-responsive" src="img/Ali-Baba-Logo.jpg" alt="logo alibaba" >
+        </a>
+      </div>
+          <ul class="nav navbar-nav">
+            <li>
+            </li>
+            <li>
+              <a  href="index.php">ACCUEIL</a>
+            </li>
+            <li>
+            <hr class="separation" />
+            </li>
+            <li>
+              <a href="#">NOS MENUS</a>
+            </li>
+            <li>
+            <hr class="separation" />
+            </li>
+            <li>
+              <a href="#">LIVRAISON</a>
+            </li>
+          </ul>
+    </div>
+  </nav>
+</body>
+</html>
