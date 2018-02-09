2dfe675aa41f039d62ef8e5dbfd1d8b4ffe0f629
Salih <gungor.salih@outlook.fr>
2018-02-09 09:12:18 +0100

    cc

--- ApplicationTransportCCAS_mobilefirstWeb/index.php
+++ ApplicationTransportCCAS_mobilefirstWeb/index.php
@@ -12,9 +12,36 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   <script src="bootstrap-v3/js/bootstrap.min.js"></script>
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+  <script src="slider/responsiveslides.min.js"></script>
 </head>
 <script>
+$(document).ready(function() {
+    $('.container').fadeIn(1000);
+});
+</script>
+<script>
+  $(function() {
+    $(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+  });
 </script>
 <header>
   <nav class="navbar navbar-default" role="navigation">
@@ -50,8 +77,22 @@
 </header>
 <body>
   <div class="container">
-
-  </div>
+    <div class="slider">
+        <ul class="rslides">
+          <li><img src="img/1.jpg" alt=""></li>
+          <li><img src="img/2.jpg" alt=""></li>
+          <li><img src="img/3.jpg" alt=""></li>
+        </ul>
+    </div>
+    <div class="meteo">
+    </div>
+    <div class="news">
+    </div>
+    <div class="cube">
+    </div>
+    <div class="cube">
+    </div>
+</div>
   <footer class="footer">
     <section class="bg-white" style="position:relative">
       <div class="container2">
