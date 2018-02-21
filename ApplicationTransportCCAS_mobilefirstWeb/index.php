deadac7dca50d3453697ef3e2ca2d1bf9f60cb83
Salih <gungor.salih@outlook.fr>
2018-02-21 17:04:44 +0100

    21/02/2018

--- ApplicationTransportCCAS_mobilefirstWeb/index.php
+++ ApplicationTransportCCAS_mobilefirstWeb/index.php
@@ -20,28 +20,6 @@ $(document).ready(function() {
     $('.container').fadeIn(1000);
 });
 </script>
-<script>
-  $(function() {
-    $(".rslides").responsiveSlides({
-  auto: true,             // Boolean: Animate automatically, true or false
-  speed: 1000,            // Integer: Speed of the transition, in milliseconds
-  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-  pager: false,           // Boolean: Show pager, true or false
-  nav: false,             // Boolean: Show navigation, true or false
-  random: false,          // Boolean: Randomize the order of the slides, true or false
-  pause: false,           // Boolean: Pause on hover, true or false
-  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
-  prevText: "Previous",   // String: Text for the "previous" button
-  nextText: "Next",       // String: Text for the "next" button
-  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
-  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
-  manualControls: "",     // Selector: Declare custom pager navigation
-  namespace: "rslides",   // String: Change the default namespace used
-  before: function(){},   // Function: Before callback
-  after: function(){}     // Function: After callback
-});
-  });
-</script>
 <header>
   <nav class="navbar navbar-default" role="navigation">
     <div class="container-fluid">
@@ -52,19 +30,10 @@ $(document).ready(function() {
       </div>
           <ul class="nav navbar-nav">
             <li>
-            </li>
-            <li>
               <a href="#">ACCUEIL</a>
             </li>
             <li>
-            <hr class="separation" />
-            </li>
-            <li>
               <a class="link" href="pages/menus.php">NOS MENUS</a>
-            </li>
-
-            <li>
-            <hr class="separation" />
             </li>
             <li>
               <a href="#" class="livraisonItem" ></i>LIVRAISON</a>
